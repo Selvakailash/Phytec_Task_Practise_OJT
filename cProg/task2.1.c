f19afb7a8c6d2f8ca1925f9bae4eb1726b099f11
Selvakailash <selvakailashramu@gmail.com>
2025-07-30 06:08:13 +0530

    assignment2

---
+++ cProg/task2.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number = 10;
+
+    if (number > 0) {
+        printf("The number is positive.\n");
+    } else if (number < 0) {
+        printf("The number is negative.\n");
+    } else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
+}
